c4b8286841f6b6c6f5c3648b4dd53162d62d4d6b
David <davidb15@ru.is>
2018-09-18 09:33:51 +0000

    if statement til þess að athuga hvert location er í leiknum komið inn, if statementið tekur svo á móti inputi um hvert eigi að fara

--- TileTraveler.py
+++ TileTraveler.py
@@ -1,6 +1,38 @@
-location = '11'
+north = '(N)orth'
+east = '(E)ast'
+west = '(W)est'
+south = '(S)outh'
+travel = 'You can travel: '
 
 for i in range(1,4):
     for j in range(1,4):
         location = str(i) + str(j)
-        print(location)
+        
+    if location == 11:
+        move = str(input(travel + north + '.'))
+
+    elif location == 12:
+        move = str(input(travel + north + ' or ' + east + ' or ' + south + '.'))
+
+    elif location == 13:
+        move = str(input(travel + east + ' or ' + south + '.'))
+
+    elif location == 21:
+        move = str(input(travel + north + '.'))
+
+    elif location == 22:
+        move = str(input(travel + south + ' or ' + west + '.'))
+
+    elif location == 23:
+        move = str(input(travel + east + ' or ' + west + '.'))
+
+    elif location == 31:
+        move = str(input(travel + south + ' or ' + west + '.'))
+
+    elif location == 32:
+        move = str(input(travel + north + ' or ' + south + '.'))
+
+    else:
+        print('Victory!')
+
+        
