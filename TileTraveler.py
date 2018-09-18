6c07b7c72f98403cfe2d1cb6bedce5d8c59f327b
David <davidb15@ru.is>
2018-09-18 10:22:20 +0000

    Verkefnið er tilbúiðþurfti að færa if lykkjur til hliðar því ég hafði tabað þær og langt út og fékk því villu

--- TileTraveler.py
+++ TileTraveler.py
@@ -13,83 +13,82 @@ while i < 4:
         if location == '11':
             print(travel + north + '.')
             direction = str(input("Direction: "))
-                if direction == 'n' or 'N':
-                    j = 2
-                else:
-                    print("Not a valid direction")
+            if direction in 'nN':
+                j = 2
+            else:
+                print("Not a valid direction")
 
         elif location == '12':
             print(travel + north + ' or ' + east + ' or ' + south + '.')
             direction = str(input("Direction: "))
-                if direction == 'n' or 'N':
-                    j = 2 
-                elif direction == 's' or 'S':
-                    j = 1
-                elif direction == 'e' or 'E':
-                    i = 2
-                else:
-                    print("Not a valid direction")
+            if direction in 'nN':
+                j = 3 
+            elif direction in 'sS':
+                j = 1
+            elif direction in 'eE':
+                i = 2
+            else:
+                print("Not a valid direction")
 
         elif location == '13':
             print(travel + east + ' or ' + south + '.')
             direction = str(input("Direction: "))
-                if direction == 'e' or 'E':
-                    i = 2
-                elif direction == 's' or 'S':
-                    j = 2
-                else:
-                    print("Not a valid direction")
+            if direction in 'eE':
+                i = 2
+            elif direction in 'sS':
+                j = 2
+            else:
+                print("Not a valid direction")
 
         elif location == '21':
             print(travel + north + '.')
             direction = str(input("Direction: "))
-                if direction == 'n' or 'N':
-                    j = 2
-                else:
-                    print("Not a valid direction")
+            if direction in 'nN':
+                j = 2
+            else:
+                print("Not a valid direction")
 
         elif location == '22':
             print(travel + south + ' or ' + west + '.')
             direction = str(input("Direction: "))
-                if direction == 's' or 'S':
-                    j = 1
-                elif direction == 'w' or 'W':
-                    i = 1
-                else:
-                    print("Not a valid direction")
+            if direction in 'sS':
+                j = 1
+            elif direction in 'wW':
+                i = 1
+            else:
+                print("Not a valid direction")
 
         elif location == '23':
             print(travel + east + ' or ' + west + '.')
             direction = str(input("Direction: "))
-                if direction == 'e' or 'E':
-                    i = 3
-                elif direction == 'w' or 'W':
-                    i = 1
-                else:
-                    print("Not a valid direction")
+            if direction in 'eE':
+                i = 3
+            elif direction in 'wW':
+                i = 1
+            else:
+                print("Not a valid direction")
 
         elif location == '33':
             print(travel + south + ' or ' + west + '.')
             direction = str(input("Direction: "))
-                if direction == 's' or 'S':
-                    j = 2
-                elif direction == 'w' or 'W':
-                    i = 2
-                else:
-                    print("Not a valid direction")
+            if direction in 'sS':
+                j = 2
+            elif direction in 'wW':
+                i = 2
+            else:
+                print("Not a valid direction")
 
         elif location == '32':
             print(travel + north + ' or ' + south + '.')
             direction = str(input("Direction: "))
-                if direction == 's' or 'S':
-                    j = 1
-                elif direction == 'n' or 'N':
-                    j = 3
-                else:
-                    print("Not a valid direction")
+            if direction in 'sS':
+                j = 1
+            elif direction in 'nN':
+                j = 3
+            else:
+                print("Not a valid direction")
                 
         else:
             print('Victory!')
             j = 5
             i = 5
-        
